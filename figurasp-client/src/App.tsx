9054d5f5f2897ab3c34e61aa8aa438f2e1852cce
Wladyslaw Figura <wfigura@op.pl>
2026-03-26 17:28:53 +0000

    01_client_init

---
+++ figurasp-client/src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div>
+      <h1>Welcome to FiguraSp</h1>
+      <p>Your go-to solution for all things figure-related.</p>
+    </div>
+  );
+}
+
+export default App;
