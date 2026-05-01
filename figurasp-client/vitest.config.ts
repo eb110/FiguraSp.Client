6dae71ae6cfa6df3748acdc8627732e13731ee2a
Wladyslaw Figura <wfigura@op.pl>
2026-05-01 16:46:37 +0100

    25_client test coverage

--- figurasp-client/vitest.config.ts
+++ figurasp-client/vitest.config.ts
@@ -4,6 +4,19 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
     plugins: [react()],
     test: {
+        coverage: {
+            provider: 'v8',
+            thresholds: {
+                lines: 80,
+                functions: 80,
+                branches: 80,
+                statements: 80,
+                // perFile: true, // Enforce thresholds per file
+            },
+            enabled: true,
+            include: ['src/**/*.{ts,tsx}'],
+            exclude: ['teamResponse.ts']
+        },
         globals: true,
         environment: 'jsdom',
         setupFiles: './test/setup.js',
